8e419d8b3706784f3ac2da6f0bbf761e0c3825aa
michael james <majames071@gmail.com>
2020-09-30 03:47:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_min_in_nested_arrays(array_of_daily_temperatures)
   count = 0
 
   while count < array_of_daily_temperatures.length do
-    p array_of_arrays[count].min
+    p array_of_daily_temperatures[count].min
     count += 1
   end
 end
